951677fee7e466a562a9ee1a7d1ad155606d5fbb
sk13 <sk13@inf.tu-dresden.de>
2020-01-26 11:18:34 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,18 @@
 /**
- * Modem functionality to send and received AT commands and responses.
+ * Functionallity to do symmetric encryption / decryption.
  *
- * @author Matthias L. Jugel
+ * 
  */
 //% weight=2 color=#f2c10d icon="\uf0ec"
 //% advanced=true
-//% parts="modem
 namespace Crypto
-{
-    // enabling DEBUG allows to follow the AT flow on the USB serial port
-    // this switches the serial back and forth and introduces delays
-    let DEBUG = false;
-    
+{  
  /**
-     * Enable AT command debug.
+     * Encrypt a message with the given key.
      */
     //% weight=1
-    //% blockId=modem_setDEBUG block="enable DEBUG %debug"
-    //% parts="modem"
-    export function enableDebug(debug: boolean = false): void {
-        DEBUG = debug;
+    //% blockId=symcrypto_encrypt block="encrypt a message %msg with key %key"
+    export function encrypt(msg: string = "",key:string=""): string {
+        return "hello world!";
     }
 }
