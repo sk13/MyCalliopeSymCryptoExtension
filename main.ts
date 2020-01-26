05139540a5b0fdabe464c1cd1a466b0df2f3d08e
sk13 <sk13@inf.tu-dresden.de>
2020-01-26 11:00:48 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
-//% color="#AA278D"
+/**
+ * Modem functionality to send and received AT commands and responses.
+ *
+ * @author Matthias L. Jugel
+ */
+//% weight=2 color=#f2c10d icon="\uf0ec"
+//% advanced=true
+//% parts="modem
 namespace Crypto
 {
-    /**
-     * This is a statement block
+    // enabling DEBUG allows to follow the AT flow on the USB serial port
+    // this switches the serial back and forth and introduces delays
+    let DEBUG = false;
+    
+ /**
+     * Enable AT command debug.
      */
-    //% block
-    export function statement()
-    {
-
+    //% weight=1
+    //% blockId=modem_setDEBUG block="enable DEBUG %debug"
+    //% parts="modem"
+    export function enableDebug(debug: boolean = false): void {
+        DEBUG = debug;
     }
 }
