ee353428bdd79b58c2f38a5e2a5c84f1d3fd9c01
sk13 <sk13@inf.tu-dresden.de>
2020-01-27 21:27:25 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -5,17 +5,16 @@
  */
 //% weight=2 color=#f2c10d icon="\uf0ec"
 //% advanced=true
-namespace Crypto
-{  
+namespace Crypto {
     let onReceivedStringHandler: (receivedString: string) => void;
- /**
-     * Encrypt a message with the given key.
-     */
+    /**
+        * Encrypt a message with the given key.
+        */
     //% weight=1
-  //% blockId=symcrypto_encrypt block="encrypts the message  %msg| with key %key"
-       export function encrypt(msg: string = "", key: string = ""): string {
-       
-           return "hello world!";
+    //% blockId=symcrypto_encrypt block="encrypts the message  %msg| with key %key"
+    export function encrypt(msg: string = "", key: string = ""): string {
+
+        return "hello world!";
     }
 
 
@@ -24,77 +23,68 @@ namespace Crypto
      */
     //% weight=1
     //% blockId=symcrypto_sendmsg block="sends the message  %msg"
-    export function sendMsg(msg: string = ""): void
-    {
-       let buff:number[] = strToBuffer(msg);
+    export function sendMsg(msg: string = ""): void {
+        let buff: number[] = strToBuffer(msg);
         let len = buff.length;
         let index = 0;
-        while (len > 19)
-        {
-            let b:Buffer=createBufferFromArray(buff, index, 19);
+        while (len > 19) {
+            let b: Buffer = createBufferFromArray(buff, index, 19);
             radio.sendBuffer(b);
             len -= 19;
             index += 19;
         }
-        if (len > 0)
-        {
+        if (len > 0) {
             let b: Buffer = createBufferFromArray(buff, index, len);
             radio.sendBuffer(b);
         }
-        
+
     }
 
-  /*  function proccessReceivedBuffer(receivedBuffer: Buffer): void
-    {
-        onReceivedStringHandler("hello");
-    }
-    */
+    /*  function proccessReceivedBuffer(receivedBuffer: Buffer): void
+      {
+          onReceivedStringHandler("hello");
+      }
+      */
     /**
     * Registers code to run when the we receive a large string.
     */
     //% blockId=crypto_on_receive_str block="on msg received"
- /*   export function onReceivedString(cb: (receivedString: string) => void)
-    {
-        radio.onReceivedBuffer(proccessReceivedBuffer);
-        onReceivedStringHandler = cb;
-    }
-    */
-    
-    function createBufferFromArray(bytes: number[], offset: number, len: number):Buffer
-    {
-        let buf:Buffer = control.createBuffer(len);
+    /*   export function onReceivedString(cb: (receivedString: string) => void)
+       {
+           radio.onReceivedBuffer(proccessReceivedBuffer);
+           onReceivedStringHandler = cb;
+       }
+       */
+
+    function createBufferFromArray(bytes: number[], offset: number, len: number): Buffer {
+        let buf: Buffer = control.createBuffer(len);
         for (let i = 0; i < len; ++i)
             buf[i] = bytes[i + offset];
         return buf;
     }
 
-    function strToBuffer(str: string): number[]
-    {
-        let utf8 = [];
-        for (var i = 0; i < str.length; i++)
-        {
-            var charcode = str.charCodeAt(i);
+    function strToBuffer(str: string): number[] {
+        let utf8: number[] = [];
+        for (let i = 0; i < str.length; i++) {
+            let charcode = str.charCodeAt(i);
             if (charcode < 0x80) utf8.push(charcode);
-            else if (charcode < 0x800)
-            {
-                utf8.push(0xc0 | (charcode >> 6),
-                    0x80 | (charcode & 0x3f));
+            else if (charcode < 0x800) {
+                utf8.push(0xc0 | (charcode >> 6));
+                utf8.push(0x80 | (charcode & 0x3f));
             }
-            else if (charcode < 0xd800 || charcode >= 0xe000)
-            {
-                utf8.push(0xe0 | (charcode >> 12),
-                    0x80 | ((charcode >> 6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
+            else if (charcode < 0xd800 || charcode >= 0xe000) {
+                utf8.push(0xe0 | (charcode >> 12));
+                utf8.push(0x80 | ((charcode >> 6) & 0x3f));
+                utf8.push(0x80 | (charcode & 0x3f));
             }
             // surrogate pair
-            else
-            {
+            else {
                 i++;
                 charcode = ((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff)
-                utf8.push(0xf0 | (charcode >> 18),
-                    0x80 | ((charcode >> 12) & 0x3f),
-                    0x80 | ((charcode >> 6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
+                utf8.push(0xf0 | (charcode >> 18));
+                utf8.push(0x80 | ((charcode >> 12) & 0x3f));
+                utf8.push(0x80 | ((charcode >> 6) & 0x3f));
+                utf8.push(0x80 | (charcode & 0x3f));
             }
         }
         return utf8;
