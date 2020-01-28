65b9ba099753e5d6ad8fba7a86fd615e8d6974b2
sk13 <sk13@inf.tu-dresden.de>
2020-01-28 09:27:59 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -27,19 +27,18 @@ namespace Crypto {
         let buff: number[] = strToBuffer(msg);
         let len: number = buff.length;
         let index: number = 0;
-         while (len > 19) {
-             let b: Buffer = createBufferFromArray(buff, index, 19);
- //            radio.sendBuffer(b);
-             len -= 19;
-             index += 19;
-         }
- 
-        if (len > 0) 
-        {
-             let b: Buffer = createBufferFromArray(buff, index, len);
-       //      radio.sendBuffer(b);
-         }
- 
+        while (len > 19) {
+            let b: Buffer = createBufferFromArray(buff, index, 19);
+            //            radio.sendBuffer(b);
+            len -= 19;
+            index += 19;
+        }
+
+        if (len > 0) {
+            let b: Buffer = createBufferFromArray(buff, index, len);
+            //      radio.sendBuffer(b);
+        }
+
     }
 
     function proccessReceivedBuffer(receivedBuffer: Buffer): void {
@@ -47,15 +46,15 @@ namespace Crypto {
     }
 
 
- /**
-    * Registers code to run when the we receive a large string.
-    */
+    /**
+       * Registers code to run when the we receive a large string.
+       */
     //% blockId=crypto_on_receive_str 
     //% block="on msg received"
     //% draggableParameters = reporter
-    export function onReceivedString(body: () => void):void {
+    export function onReceivedString(body: (msg: string) => void): void {
         //radio.onReceivedBuffer(proccessReceivedBuffer);
-       // onReceivedStringHandler = cb;
+        // onReceivedStringHandler = cb;
     }
 
     function createBufferFromArray(bytes: number[], offset: number, len: number): Buffer {
