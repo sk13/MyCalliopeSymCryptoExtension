db817df37574c330a5c33d530b8806d08130264f
sk13 <sk13@inf.tu-dresden.de>
2020-01-28 15:41:04 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -58,17 +58,18 @@ namespace Crypto {
     function proccessReceivedPacket(packet: radio.Packet): void 
     {
         let s: string = packet.receivedString;
-        if(s.length>0)
-            {
-                lastMsg+=s;
-                return;
-            }
-        let n:number=packet.receivedNumber;
-        if(n>0&&n==lastMsg.length)
-            {    
-                onReceivedStringHandler();
-                lastMsg="";
-            }
+        if (s.length > 0) {
+            lastMsg += s;
+            return;
+        }
+        let n: number = packet.receivedNumber;
+        if (n > 0 && n == lastMsg.length) 
+        {
+            let bytes:number[]=decodeBinary(lastMsg);
+            lastMsg=UTF8toStr(bytes);
+            onReceivedStringHandler();
+            lastMsg = "";
+        }
     }
 
 
@@ -90,7 +91,8 @@ namespace Crypto {
         return buf;
     }
 
-    function encodeBinary(bytes: number[]): string {
+    function encodeBinary(bytes: number[]): string 
+    {
         let s: string = "";
         let i: number = 0;
         for (i = 0; i < bytes.length; i++) {
@@ -99,7 +101,50 @@ namespace Crypto {
         return s;
     }
 
-    function strToUTF8(str: string): number[] {
+    function decodeBinary(str: string): number[] 
+    {
+        let s: string = "";
+        let i: number = 0;
+        let bytes:number[]=[];
+        for (i = 0; i < str.length; i++) 
+        {
+            let b:number=str.charCodeAt(i);
+            bytes.push(b);
+        }
+        return bytes;
+    }
+
+
+function UTF8toStr(bytes:number[]):string
+{
+    let len:number =bytes.length;
+    let str:string="";
+    let i:number=0;
+    let b1:number;
+    let b2:number;
+    while(len>0)
+    {
+        b1=bytes[i++];
+        if(b1<0x80)
+        {
+            str+=String.fromCharCode(b1);
+        }
+        else
+        {
+            b2=bytes[i++];
+            let code:number=b1-0xC0;
+            code<<=6;
+            b2-=0x80;
+            code+=b2;
+            str += String.fromCharCode(b2);    
+        }
+        
+    }
+    return str;
+}
+
+    function strToUTF8(str: string): number[]
+     {
         let utf8: number[] = [];
         for (let i = 0; i < str.length; i++) {
             let charcode = str.charCodeAt(i);
