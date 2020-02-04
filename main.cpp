713d6e4e62fa7736fa43b79f2ad5cdd9044491e6
sk13 <sk13@inf.tu-dresden.de>
2020-02-04 19:47:56 +0100

    Updates.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,22 @@ void sendRawPacket(Buffer msg)
 {
     //uBit.radio.datagram.send(msg->data, msg->len);
 }
-
+ 
+/**
+     * Broadcasts a number over radio to any connected micro:bit in the group.
+     */
+    //% weight=60
+    //% blockId=serial_setrxbuffersize
 void setRxBufferSize(int i)
 {
     uBit.serial.setRxBufferSize(i);
 }
 
+/**
+     * Broadcasts a number over radio to any connected micro:bit in the group.
+     */
+    //% weight=60
+    //% blockId=serial_getrxbuffersize
 int getRxBufferSize()
 {
     return uBit.serial.getRxBufferSize();
--- shims.d.ts
+++ shims.d.ts
@@ -1,22 +1,19 @@
+// Auto-generated. Do not edit.
 declare namespace Crypto {
 
- 
-    //% async shim=Crypto::sendRawPacket
-    function sendRawPacket(msg: Buffer): void;
-
-/**
+    /**
      * Broadcasts a number over radio to any connected micro:bit in the group.
      */
     //% weight=60
-    //% blockId=serial_getrxbuffersize
-    //% shim = Crypto::getRxBufferSize
-    function getRxBufferSize():number;
+    //% blockId=serial_setrxbuffersize shim=Crypto::setRxBufferSize
+    function setRxBufferSize(i: int32): void;
 
-/**
+    /**
      * Broadcasts a number over radio to any connected micro:bit in the group.
      */
     //% weight=60
-    //% blockId=serial_setrxbuffersize
-    //% shim = Crypto::setRxBufferSize
-    function setRxBufferSize(size:number):void;
-    }
+    //% blockId=serial_getrxbuffersize shim=Crypto::getRxBufferSize
+    function getRxBufferSize(): int32;
+}
+
+// Auto-generated. Do not edit. Really.
